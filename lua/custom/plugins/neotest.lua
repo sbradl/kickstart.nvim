a3dec5f6c1e5fc71f7b160f214ccfc623c11b0e3
Stefan Bradl <bradlstefan@googlemail.com>
2025-09-07 21:46:56 +0200

    added neotest shortcuts

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -7,13 +7,43 @@ return {
       'antoinemadec/FixCursorHold.nvim',
       'nvim-treesitter/nvim-treesitter',
       'jfpedroza/neotest-elixir',
+      'Issafalcon/neotest-dotnet',
+      'marilari88/neotest-vitest',
     },
     config = function()
       require('neotest').setup {
         adapters = {
           require 'neotest-elixir',
+          require 'neotest-dotnet' {
+            dap = {
+              args = { justMyCode = false },
+              adapter_name = 'netcoredbg',
+              discovery_root = 'solution',
+            },
+          },
+          require 'neotest-vitest' {
+            filter_dir = function(name, rel_path, root)
+              return name ~= 'node_modules'
+            end,
+          },
         },
       }
+
+      vim.keymap.set('n', '<leader>tt', function()
+        require('neotest').run.run(vim.fn.expand '%')
+      end, { desc = 'Run tests in current file' })
+
+      vim.keymap.set('n', '<leader>tr', function()
+        require('neotest').run.run()
+      end)
+
+      vim.keymap.set('n', '<leader>to', function()
+        require('neotest').output.open { enter = true }
+      end)
+
+      vim.keymap.set('n', '<leader>ts', function()
+        require('neotest').summary.toggle()
+      end)
     end,
   },
 }
