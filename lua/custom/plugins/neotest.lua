09df96514789cbc581ad407519d1b2d3b6e2a597
Stefan Bradl <bradlstefan@googlemail.com>
2025-09-08 11:46:27 +0200

    changed from neotest-dotnet to neotest-vstest because the former could not execute tests

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -1,25 +1,27 @@
 return {
   {
     'nvim-neotest/neotest',
+    commit = '52fca6717ef972113ddd6ca223e30ad0abb2800c',
     dependencies = {
       'nvim-neotest/nvim-nio',
       'nvim-lua/plenary.nvim',
       'antoinemadec/FixCursorHold.nvim',
       'nvim-treesitter/nvim-treesitter',
       'jfpedroza/neotest-elixir',
-      'Issafalcon/neotest-dotnet',
+      'nsidorenco/neotest-vstest',
       'marilari88/neotest-vitest',
     },
     config = function()
       require('neotest').setup {
         adapters = {
           require 'neotest-elixir',
-          require 'neotest-dotnet' {
-            dap = {
-              args = { justMyCode = false },
-              adapter_name = 'netcoredbg',
-              discovery_root = 'solution',
+          require 'neotest-vstest' {
+            dap_settings = {
+              type = 'netcoredbg',
             },
+            solution_selector = function()
+              return nil
+            end,
           },
           require 'neotest-vitest' {
             filter_dir = function(name, rel_path, root)
